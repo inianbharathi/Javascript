936141d199767512ad3f9396567b8e2270a0832d
Inian Bharathi <inianbharathi@gmail.com>
2024-01-24 06:02:21 +0530

    Add files via upload

--- love_calculator.js
+++ love_calculator.js
@@ -1,5 +1,14 @@
 prompt("Enter your name: ");
 prompt("Enter your partner name: ");
 var n = Math.random();
-n= (Math.floor(n*100)+1) + "%";
-alert("Your Love Score: "+ n);
+n= (Math.floor(n*100)+1);
+if (n>70)
+{
+    alert("You are made for each other: "+n +"%");
+}
+if (n>=30&& n<70){
+    alert("You are ok with each other "+n+"%");
+}
+if(n<=30){
+    alert("You don't get along "+n+"%");
+}
