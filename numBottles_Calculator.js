2b76c57bff2ce870a59f2cb73b32caff52424c33
Inian Bharathi <inianbharathi@gmail.com>
2024-04-19 05:28:43 +0530

    Add files via upload

---
+++ numBottles_Calculator.js
@@ -0,0 +1,24 @@
+function getMilk(money) {
+    var cost=1.5;
+    var numBottles=Math.floor(money/cost);
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("buy"+ numBottles+ " bottles of Milk");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(5);
