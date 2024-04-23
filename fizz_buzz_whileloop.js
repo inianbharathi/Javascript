9d3edb1a7bbd5e41fa501f7b612ca844f17efb69
Inian Bharathi <inianbharathi@gmail.com>
2024-04-23 16:00:05 +0530

    Add files via upload

---
+++ fizz_buzz_whileloop.js
@@ -0,0 +1,28 @@
+var output=[];
+var count=1;
+function fizzbuzz(){
+
+    while(count<=100){
+     if ((count%3===0) && (count%5===0))
+    {
+        output.push("FizzBuZZ");
+    }
+    else if (count%3===0)
+    {
+        output.push("Fizz");
+    }
+    else if (count%5===0)
+    {
+        output.push("Buzz");        
+    }
+   
+    else
+    {
+        output.push(count);
+    }
+    
+    count++;
+    }
+    console.log(output);
+}
+fizzbuzz();
